b6e200b4224546ecada594ed2ca7eca48c236073
Kimun Kim <kkm97351@gmail.com>
2021-11-10 06:34:28 +0900

    AOP 프로젝트 실습

--- aop/src/test/java/hello/aop/AopTest.java
+++ aop/src/test/java/hello/aop/AopTest.java
@@ -2,16 +2,22 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.AspectV4;
+import hello.aop.order.aop.AspectV5Order;
+import hello.aop.order.aop.AspectV6Advice;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 @SpringBootTest
+@Import(AspectV6Advice.class)
 public class AopTest {
 
     @Autowired
