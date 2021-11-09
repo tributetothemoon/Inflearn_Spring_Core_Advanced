c1ac1d432190ed457985c1c0571e906e8ae22301
Kimun Kim <kkm97351@gmail.com>
2021-11-10 01:39:42 +0900

    ProxyFactory 적용

--- proxy-start/src/test/java/hello/proxy/common/service/ServiceImpl.java
+++ proxy-start/src/test/java/hello/proxy/common/service/ServiceImpl.java
@@ -3,7 +3,7 @@ package hello.proxy.common.service;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class ServiceInterfaceImpl implements ServiceInterface {
+public class ServiceImpl implements ServiceInterface {
     @Override
     public void save() {
         log.info("save 호출");
